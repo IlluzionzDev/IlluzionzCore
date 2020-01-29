d1fbad54ab953677b422abc89a48a41a90206ec3
IlluzionzDev <jaminstratford@gmail.com>
2020-01-29 23:09:00 +1300

    Added player util class

---
+++ Core/src/main/java/com/illuzionzstudios/core/util/PlayerUtil.java
@@ -0,0 +1,14 @@
+package com.illuzionzstudios.core.util;
+
+/**
+ * Copyright © 2020 Property of Illuzionz Studios, LLC
+ * All rights reserved. No part of this publication may be reproduced, distributed, or
+ * transmitted in any form or by any means, including photocopying, recording, or other
+ * electronic or mechanical methods, without the prior written permission of the publisher,
+ * except in the case of brief quotations embodied in critical reviews and certain other
+ * noncommercial uses permitted by copyright law. Any licensing of this software overrides
+ * this statement.
+ */
+
+public class PlayerUtil {
+}
