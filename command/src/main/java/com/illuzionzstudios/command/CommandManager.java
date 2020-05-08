830b834a2f1b360fd8344cf2768ad3999a3df928
IlluzionzDev <jaminstratford@gmail.com>
2020-05-09 10:04:21 +1200

    Heavily optimise commands

--- command/src/main/java/com/illuzionzstudios/command/CommandManager.java
+++ command/src/main/java/com/illuzionzstudios/command/CommandManager.java
@@ -1,11 +1,7 @@
 package com.illuzionzstudios.command;
 
-import com.illuzionzstudios.command.type.BaseCommand;
-import com.illuzionzstudios.command.type.GlobalCommand;
-import com.illuzionzstudios.command.type.PlayerCommand;
+import com.illuzionzstudios.command.type.AbstractCommand;
 import com.illuzionzstudios.core.bukkit.controller.BukkitController;
-import com.illuzionzstudios.core.plugin.IlluzionzPlugin;
-import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
@@ -15,44 +11,23 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.SimplePluginManager;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- * Copyright © 2020 Property of Illuzionz Studios, LLC
- * All rights reserved. No part of this publication may be reproduced, distributed, or
- * transmitted in any form or by any means, including photocopying, recording, or other
- * electronic or mechanical methods, without the prior written permission of the publisher,
- * except in the case of brief quotations embodied in critical reviews and certain other
- * noncommercial uses permitted by copyright law. Any licensing of this software overrides
- * this statement.
- */
-
-/**
- * Manage all commands
- */
 public class CommandManager implements BukkitController<Plugin>, Listener {
 
     /**
      * Instance of command manager for registering commands
      */
-    private static CommandManager INSTANCE = null;
+    private static CommandManager INSTANCE;
 
     /**
-     * Registered player commands
+     * @return Singleton instance of command manager
      */
-    protected ArrayList<PlayerCommand> pCommands = new ArrayList<>();
-
-    /**
-     * Registered global commands
-     */
-    protected ArrayList<GlobalCommand> gCommands = new ArrayList<>();
-
-    public CommandManager(Plugin plugin) {
-        INSTANCE = this;
-    }
-
     public static CommandManager get() {
+        if (INSTANCE == null) {
+            INSTANCE = new CommandManager();
+        }
+
         return INSTANCE;
     }
 
@@ -63,7 +38,6 @@ public class CommandManager implements BukkitController<Plugin>, Listener {
 
     @Override
     public void stop(Plugin plugin) {
-
     }
 
     /**
@@ -71,21 +45,25 @@ public class CommandManager implements BukkitController<Plugin>, Listener {
      *
      * @param command Base command
      */
-    public void register(BaseCommand command) {
+    public void register(AbstractCommand command) {
         try {
-            //Prepare command map
+            // Prepare command map
             Field cMap = SimplePluginManager.class.getDeclaredField("commandMap");
             cMap.setAccessible(true);
             CommandMap map = (CommandMap) cMap.get(Bukkit.getPluginManager());
 
 
-            //Remove existing commands
+            // Remove existing commands
             Field knownCommandsField = SimpleCommandMap.class.getDeclaredField("knownCommands");
+
             knownCommandsField.setAccessible(true);
             HashMap<String, Command> knownCommands = (HashMap<String, Command>) knownCommandsField.get(map);
+            knownCommandsField.setAccessible(false);
+
             if (map.getCommand(command.getLabel()) != null) {
                 knownCommands.remove(command.getLabel());
             }
+
             for (String alias : command.getAliases()) {
                 if (map.getCommand(alias) != null) {
                     knownCommands.remove(alias);
@@ -97,12 +75,6 @@ public class CommandManager implements BukkitController<Plugin>, Listener {
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
-
-        // Register our player and global commands
-        if (command instanceof PlayerCommand) {
-            pCommands.add((PlayerCommand) command);
-        } else if (command instanceof GlobalCommand) {
-            gCommands.add((GlobalCommand) command);
-        }
     }
+
 }
